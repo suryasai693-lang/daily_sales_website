28b58a026438935d58abb5546c08a0fab87417ba
Suryasai693 <suryasai693@gmail.com>
2026-08-27 16:21:22 +0530

    Fix public folder path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 // =====================================================
